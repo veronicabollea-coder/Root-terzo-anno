691438eb67b6d21abc9a17ef983e20b558c240c2
veronicabollea-coder <veronica.bollea@edu.unito.it>
2026-03-31 14:53:24 +0000

    ok

--- graficoRate.C
+++ graficoRate.C
@@ -5,28 +5,29 @@
 void graficoRate() {
 
     // --- DATI DI ESEMPIO ---
-    const int N = 3;
-    double x[N]     = {9.8, 19.5, 29.9};        // variabile indipendente
-    double y[N]     = {0.45, 0.55, 1.24}; // variabile misurata
-    double ex[N]    = {0, 0, 0};        // errori in x (qui nulli)
-    double ey[N]    = {0.03, 0.03, 0.07}; // errori verticali
+    const int N = 2;
+    double x[N]     = {9.8, 19.5};        // variabile indipendente
+    double y[N]     = {2.7, 1.21 }; // variabile misurata
+    double ex[N]    = {0, 0};        // errori in x (qui nulli)
+    double ey[N]    = {0.3, 0.15 }; // errori verticali
 
     // --- CREAZIONE DEL GRAFICO CON ERRORI ---
     TGraphErrors* gr = new TGraphErrors(N, x, y, ex, ey);
 
-    gr->SetTitle("Rate in funzione della distanza;d[cm];R(d)[Hz]");
+    gr->SetTitle("Rate normalizzato in funzione della distanza;d[cm];R(d)/R(30)");
     gr->SetMarkerStyle(20);
     gr->SetMarkerSize(1.2);
-    gr->SetLineColor(kBlue + 1);
-    gr->SetMarkerColor(kBlue + 1);
+    gr->SetLineColor(1);
+    gr->SetMarkerColor(1);
 
     // --- CANVAS ---
-    TCanvas* c = new TCanvas("c", "Rate in funzione di d", 800, 600);
+    TCanvas* c = new TCanvas("c", "Rate normalizzato in funzione di d", 800, 600);
     c->SetGrid();
 
     // --- DISEGNO ---
     gr->Draw("AP"); // A = assi, P = punti (+ barre)
 
-    c->Update()
-    c->Print();
+    c->Update();
+    c->Print("gabibbo.png", "png");
+
 }
